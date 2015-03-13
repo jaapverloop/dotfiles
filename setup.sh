36e94d25d5f4eaca9c96fc97155126d942a6793f
Jaap Verloop <j.verloop@gmail.com>
2015-03-13 11:42:28 +0100

    Do not use pushd/popd in setup script.

--- setup.sh
+++ setup.sh
@@ -2,39 +2,35 @@
 
 SCRIPTPATH=$(cd "$(dirname "${0}")"; echo $(pwd))
 
-pushd $SCRIPTPATH
-
 # load submodules
 git submodule init
 git submodule update
 
 # setup base16-shell
 rm -rf ~/.base16-shell
-ln -s `pwd`/base16-shell ~/.base16-shell
+ln -s $SCRIPTPATH/base16-shell ~/.base16-shell
 
 # setup antigen
 rm -rf ~/.antigen
-ln -s `pwd`/antigen ~/.antigen
+ln -s $SCRIPTPATH/antigen ~/.antigen
 
 # setup zsh
 rm ~/.zshrc
-ln -s `pwd`/zshrc ~/.zshrc
+ln -s $SCRIPTPATH/zshrc ~/.zshrc
 
 # setup vim
 rm -rf ~/.vim
 rm ~/.vimrc
-ln -s `pwd`/vim ~/.vim
-ln -s `pwd`/vimrc ~/.vimrc
+ln -s $SCRIPTPATH/vim ~/.vim
+ln -s $SCRIPTPATH/vimrc ~/.vimrc
 vim +BundleInstall +qall &> /dev/null &
 
 # setup git
 rm ~/.gitconfig
 rm ~/.gitignore
-ln -s `pwd`/gitconfig ~/.gitconfig
-ln -s `pwd`/gitignore ~/.gitignore
+ln -s $SCRIPTPATH/gitconfig ~/.gitconfig
+ln -s $SCRIPTPATH/gitignore ~/.gitignore
 
 # setup tmux
 rm ~/.tmux.conf
-ln -s `pwd`/tmux.conf ~/.tmux.conf
-
-popd
+ln -s $SCRIPTPATH/tmux.conf ~/.tmux.conf

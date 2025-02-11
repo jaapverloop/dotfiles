7414824f6be1fcc6fa77c5397a923002a0ccd073
Jaap Verloop <j.verloop@gmail.com>
2025-02-11 14:01:04 +0100

    Remove setting which is already the default

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -4,7 +4,6 @@ return {
       dependencies = { 'nvim-tree/nvim-web-devicons' },
       config = function()
         require('oil').setup {
-          columns = { 'icon' },
           view_options = {
             show_hidden = true,
           },

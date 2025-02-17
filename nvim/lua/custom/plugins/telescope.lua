2df7930af94a9bc06d57267060a2fa8d9f4690d3
Jaap Verloop <j.verloop@gmail.com>
2025-02-17 14:47:52 +0100

    Use ivy theme as default

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -3,6 +3,10 @@ return {
   tag = '0.1.8',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
+    require('telescope').setup {
+      defaults = require('telescope.themes').get_ivy()
+    }
+
     local builtin = require('telescope.builtin')
 
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})

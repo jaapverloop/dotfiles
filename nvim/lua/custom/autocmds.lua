90ee63b9ffeb27c8d1d6cef758330e5c7aa8e4ee
Jaap Verloop <j.verloop@gmail.com>
2025-02-27 11:22:08 +0100

    Remove autocommand

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -1,13 +1,3 @@
--- removes trailing white space on save
-vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-  pattern = {'*'},
-  callback = function()
-    local save_cursor = vim.fn.getpos('.')
-    pcall(function() vim.cmd [[%s/\s\+$//e]] end)
-    vim.fn.setpos('.', save_cursor)
-  end,
-})
-
 -- only show the cursorline in current window
 vim.cmd [[
   autocmd InsertLeave,WinEnter * set cursorline

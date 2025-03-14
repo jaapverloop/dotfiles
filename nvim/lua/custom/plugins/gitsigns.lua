e6cdc0aee14aa4f57f5785c57efff5fdd1fdfcdb
Jaap Verloop <j.verloop@gmail.com>
2025-03-14 12:12:33 +0100

    Add shortcuts to make it easier to navigate between changes in buffer

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -2,7 +2,33 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup({
-      current_line_blame = true
+      on_attach = function(bufnr)
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- navigate to previous change
+        map('n', '[c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({'[c', bang = true})
+          else
+            gitsigns.nav_hunk('prev')
+          end
+        end)
+
+        -- navigate to next change
+        map('n', ']c', function()
+          if vim.wo.diff then
+            vim.cmd.normal({']c', bang = true})
+          else
+            gitsigns.nav_hunk('next')
+          end
+        end)
+      end,
     })
   end,
 }

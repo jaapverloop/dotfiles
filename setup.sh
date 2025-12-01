7e2c71ebfb4f89b3faa2bbc37aa2c82c793d7087
Jaap Verloop <j.verloop@gmail.com>
2025-12-01 14:23:19 +0100

    use the correct fabric 3

--- setup.sh
+++ setup.sh
@@ -140,7 +140,8 @@ pushd ${HOME}
 
         pip install --upgrade pip
         pip install ansible
-        pip install Fabric3
+        pip install fabric
+        pip install invoke
         pip install python-dotenv
         pip install Sphinx
         pip install sphinx-rtd-theme

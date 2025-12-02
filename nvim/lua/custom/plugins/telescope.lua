e91e617dd15cd5ba8278fce523eeda0c0cd501d8
Jaap Verloop <j.verloop@gmail.com>
2025-12-02 14:16:37 +0100

    Update and finetune telescope

--- nvim/lua/custom/plugins/telescope.lua
+++ nvim/lua/custom/plugins/telescope.lua
@@ -1,15 +1,25 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  tag = 'v0.2.0',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local telescope = require('telescope')
     local themes = require('telescope.themes')
-    local actions = require("telescope.actions")
+    local actions = require('telescope.actions')
     local builtin = require('telescope.builtin')
 
     telescope.setup {
-      defaults = themes.get_ivy(),
+      defaults = vim.tbl_deep_extend('force', themes.get_ivy(), {
+        mappings = {
+          i = {
+            ['<C-l>'] = actions.send_to_loclist,
+          },
+          n = {
+            ['<C-l>'] = actions.send_to_loclist,
+          },
+        },
+      }),
+
       pickers = {
         buffers = {
           mappings = {
@@ -21,11 +31,11 @@ return {
     }
 
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find)
     vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     vim.keymap.set('n', '<leader>fl', builtin.loclist, {})
     vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
-    vim.keymap.set('n', '<leader>fc', builtin.current_buffer_fuzzy_find)
   end,
 }

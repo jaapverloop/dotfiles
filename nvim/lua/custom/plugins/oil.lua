cdd6a406020d5ae5706605b14a044bd6f35a3c4a
Jaap Verloop <j.verloop@gmail.com>
2025-12-04 14:35:05 +0100

    Improve oil setup

--- nvim/lua/custom/plugins/oil.lua
+++ nvim/lua/custom/plugins/oil.lua
@@ -2,6 +2,7 @@ return {
   {
     'stevearc/oil.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
+    lazy = false,
     config = function()
       require('oil').setup {
         view_options = {
@@ -10,10 +11,19 @@ return {
       }
 
       -- Open parent directory in current window
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>')
 
-      -- Open parent directory in floating window
-      vim.keymap.set('n', '<space>-', require('oil').toggle_float)
+      -- Open parent directory in horizontal split
+      vim.keymap.set('n', '<leader>os', function()
+        vim.cmd('split')
+        vim.cmd('Oil')
+      end)
+
+      -- Open parent directory in vertical split
+      vim.keymap.set('n', '<leader>ov', function()
+        vim.cmd('vsplit')
+        vim.cmd('Oil')
+      end)
     end,
   },
 }

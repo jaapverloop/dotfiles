2d7d2772d2625ca9fa2fdb315ee9f208711dff0a
Jaap Verloop <j.verloop@gmail.com>
2025-12-04 14:34:48 +0100

    Show current line blame

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -2,6 +2,12 @@ return {
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup({
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text_pos = 'right_align',
+        delay = 1000,
+        ignore_whitespace = false,
+      },
       on_attach = function(bufnr)
         local gitsigns = require('gitsigns')
 

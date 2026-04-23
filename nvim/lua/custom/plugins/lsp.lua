c48c4e0635f5dd78dd1aabeb8346fd1c250bad71
Jaap Verloop <j.verloop@gmail.com>
2026-04-23 09:42:55 +0200

    Add missing opts argument

--- nvim/lua/custom/plugins/lsp.lua
+++ nvim/lua/custom/plugins/lsp.lua
@@ -43,7 +43,7 @@ return {
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+          vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
         end,
       })

2bb68eae2ebe17a9459aa72b6ea2039cd6f66bd6
Jaap Verloop <j.verloop@gmail.com>
2026-04-23 09:46:00 +0200

    Improve autocmds config

--- nvim/lua/custom/autocmds.lua
+++ nvim/lua/custom/autocmds.lua
@@ -1,11 +1,19 @@
+local augroup = vim.api.nvim_create_augroup('custom', { clear = true })
+
 -- only show the cursorline in current window
-vim.cmd [[
-  autocmd InsertLeave,WinEnter * set cursorline
-  autocmd InsertEnter,WinLeave * set nocursorline
-]]
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+  group = augroup,
+  callback = function() vim.opt.cursorline = true end,
+})
+
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+  group = augroup,
+  callback = function() vim.opt.cursorline = false end,
+})
 
 -- show diagnostic popup when cursor stops on line
 vim.api.nvim_create_autocmd('CursorHold', {
+  group = augroup,
   callback = function()
     vim.diagnostic.open_float(nil, { focus = false })
   end,

20f9b9c2167074524c56d4abf12358cb45d135c6
Jaap Verloop <j.verloop@gmail.com>
2026-06-22 08:41:41 +0200

    Remove shortcuts gitsigns

--- nvim/lua/custom/plugins/gitsigns.lua
+++ nvim/lua/custom/plugins/gitsigns.lua
@@ -8,33 +8,6 @@ return {
         delay = 1000,
         ignore_whitespace = false,
       },
-      on_attach = function(bufnr)
-        local gitsigns = require('gitsigns')
-
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
-
-        -- navigate to previous change
-        map('n', '[c', function()
-          if vim.wo.diff then
-            vim.cmd.normal({'[c', bang = true})
-          else
-            gitsigns.nav_hunk('prev')
-          end
-        end)
-
-        -- navigate to next change
-        map('n', ']c', function()
-          if vim.wo.diff then
-            vim.cmd.normal({']c', bang = true})
-          else
-            gitsigns.nav_hunk('next')
-          end
-        end)
-      end,
     })
   end,
 }
